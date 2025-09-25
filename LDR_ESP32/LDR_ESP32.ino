0dc2e57a3a1b374cb533a47263cf2d56ed1a5d5e
KanjiSaiko <hbortolomiol2@gmail.com>
2025-09-25 01:41:50 -0300

    correcao do alfabeto

--- LDR_ESP32/LDR_ESP32.ino
+++ LDR_ESP32/LDR_ESP32.ino
@@ -5,35 +5,40 @@ struct ChaveValor{
 };
 
 ChaveValor Alfabeto[26] = {
-  {{1,1,1,1,0}, 'A'}, 
-  {{0,0,0,0,1}, 'B'}, 
-  {{1,1,1,1,1}, 'C'}, 
-  {{1,1,1,0,1}, 'D'}, 
-  {{1,1,1,1,1}, 'E'}, 
-  {{0,0,0,1,0}, 'F'}, 
-  {{1,1,1,0,1}, 'G'}, 
-  {{1,1,0,0,1}, 'H'}, 
-  {{0,1,1,1,1}, 'I'},
-  {{0,1,1,1,1}, 'J'}, 
+  {{0,0,0,0,1}, 'A'}, 
+  {{1,1,1,1,0}, 'B'}, 
+  {{0,0,0,0,0}, 'C'}, 
+  {{0,0,0,0,0}, 'Ç'}, 
+  {{0,0,0,1,0}, 'D'}, 
+  {{0,0,0,0,0}, 'E'}, 
+  {{1,1,1,0,1}, 'F'}, 
+  {{0,0,0,1,0}, 'G'}, 
+  {{0,0,1,1,0}, 'H'}, 
+  {{1,0,0,0,0}, 'I'},
+  {{1,0,0,0,0}, 'J'}, 
   {{0,0,1,1,1}, 'K'}, 
-  {{1,1,1,0,0}, 'L'}, 
-  {{1,0,0,0,1}, 'M'}, 
-  {{1,1,0,0,1}, 'N'}, 
-  {{1,1,1,1,1}, 'O'}, 
-  {{1,1,0,0,0}, 'P'}, 
-  {{1,1,1,0,1}, 'Q'}, 
-  {{1,1,0,0,1}, 'R'}, 
-  {{1,1,1,1,1}, 'S'},
-  {{0,0,0,1,1}, 'T'}, 
-  {{1,1,0,0,0}, 'U'}, 
-  {{1,1,0,0,0}, 'V'}, 
-  {{1,0,0,0,1}, 'W'}, 
-  {{1,1,1,0,1}, 'X'}, 
-  {{0,1,1,1,0}, 'Y'}, 
-  {{1,1,1,0,1}, 'Z'}
+  {{0,0,0,1,1}, 'L'}, 
+  {{0,1,1,1,0}, 'M'}, 
+  {{0,0,1,1,0}, 'N'}, 
+  {{0,0,0,0,0}, 'O'}, 
+  {{0,0,1,1,1}, 'P'}, 
+  {{0,0,0,1,1}, 'Q'}, 
+  {{0,0,1,1,0}, 'R'}, 
+  {{0,0,0,0,0}, 'S'},
+  {{1,1,1,0,0}, 'T'}, 
+  {{0,0,1,1,0}, 'U'}, 
+  {{0,0,1,1,0}, 'V'}, 
+  {{0,1,1,1,0}, 'W'}, 
+  {{0,0,0,1,0}, 'X'}, 
+  {{1,0,0,0,1}, 'Y'}, 
+  {{0,0,0,1,0}, 'Z'}
 };
 
-const int LDR1 = 34;
+const int LDR1 = 32; //D32
+const int LDR2 = 33; //D33
+const int LDR3 = 34; //D34
+const int LDR4 = 35; //D35
+const int LDR5 = 36; //UP
 
 //comparação da saída dos LDR com a chave das letras do alfabeto
 bool comparaArrays(byte arr1[], byte arr2[], int tamanho_chave){
@@ -49,10 +54,34 @@ void setup() {
   Serial.begin(115200);
 }
 
-// the loop function runs over and over again forever
+
 void loop() {
+  //LEITURA DOS PINOS
   int valor_LDR1 = analogRead(LDR1);
-  Serial.print(valor_LDR1);
-  Serial.print("\n");
-  delay(3000);
+  int valor_LDR2 = analogRead(LDR2);
+  int valor_LDR3 = analogRead(LDR3);
+  int valor_LDR4 = analogRead(LDR4);
+  int valor_LDR5 = analogRead(LDR5);
+  //METRICA PARA DECIDIR QUANDO DEDO ESTA ABAIXADO OU LEVANTADO (0 OU 1)
+  (valor_LDR1 > 4000) ? Saida[0] = 0 : Saida[0] = 1;
+  (valor_LDR2 > 4000) ? Saida[1] = 0 : Saida[1] = 1;
+  (valor_LDR3 > 4000) ? Saida[2] = 0 : Saida[2] = 1;
+  (valor_LDR4 > 4000) ? Saida[3] = 0 : Saida[3] = 1;
+  (valor_LDR5 > 4000) ? Saida[4] = 0 : Saida[4] = 1;
+  Serial.print("Codigo: ");
+  Serial.print(Saida[0]);
+  Serial.print(Saida[1]);
+  Serial.print(Saida[2]);
+  Serial.print(Saida[3]);
+  Serial.print(Saida[4]);
+  Serial.print("\n=============\n");
+  //PRINTA APENAS LETRAS CORRESPONDENTES
+  for(int i=0;i<26;i++){
+    if(comparaArrays(Saida, Alfabeto[i].chave, 5)){
+      Serial.print("Letra: ");
+      Serial.print(Alfabeto[i].valor);
+      Serial.print("\n");
+    }
+  }
+  delay(1000);
 }
--- LDR_Mega2560/LDR.ino
+++ LDR_Mega2560/LDR.ino
@@ -5,32 +5,33 @@ struct ChaveValor{
 };
 
 ChaveValor Alfabeto[26] = {
-  {{1,1,1,1,0}, 'A'}, 
-  {{0,0,0,0,1}, 'B'}, 
-  {{1,1,1,1,1}, 'C'}, 
-  {{1,1,1,0,1}, 'D'}, 
-  {{1,1,1,1,1}, 'E'}, 
-  {{0,0,0,1,0}, 'F'}, 
-  {{1,1,1,0,1}, 'G'}, 
-  {{1,1,0,0,1}, 'H'}, 
-  {{0,1,1,1,1}, 'I'},
-  {{0,1,1,1,1}, 'J'}, 
+  {{0,0,0,0,1}, 'A'}, 
+  {{1,1,1,1,0}, 'B'}, 
+  {{0,0,0,0,0}, 'C'}, 
+  {{0,0,0,0,0}, 'Ç'}, 
+  {{0,0,0,1,0}, 'D'}, 
+  {{0,0,0,0,0}, 'E'}, 
+  {{1,1,1,0,1}, 'F'}, 
+  {{0,0,0,1,0}, 'G'}, 
+  {{0,0,1,1,0}, 'H'}, 
+  {{1,0,0,0,0}, 'I'},
+  {{1,0,0,0,0}, 'J'}, 
   {{0,0,1,1,1}, 'K'}, 
-  {{1,1,1,0,0}, 'L'}, 
-  {{1,0,0,0,1}, 'M'}, 
-  {{1,1,0,0,1}, 'N'}, 
-  {{1,1,1,1,1}, 'O'}, 
-  {{1,1,0,0,0}, 'P'}, 
-  {{1,1,1,0,1}, 'Q'}, 
-  {{1,1,0,0,1}, 'R'}, 
-  {{1,1,1,1,1}, 'S'},
-  {{0,0,0,1,1}, 'T'}, 
-  {{1,1,0,0,0}, 'U'}, 
-  {{1,1,0,0,0}, 'V'}, 
-  {{1,0,0,0,1}, 'W'}, 
-  {{1,1,1,0,1}, 'X'}, 
-  {{0,1,1,1,0}, 'Y'}, 
-  {{1,1,1,0,1}, 'Z'}
+  {{0,0,0,1,1}, 'L'}, 
+  {{0,1,1,1,0}, 'M'}, 
+  {{0,0,1,1,0}, 'N'}, 
+  {{0,0,0,0,0}, 'O'}, 
+  {{0,0,1,1,1}, 'P'}, 
+  {{0,0,0,1,1}, 'Q'}, 
+  {{0,0,1,1,0}, 'R'}, 
+  {{0,0,0,0,0}, 'S'},
+  {{1,1,1,0,0}, 'T'}, 
+  {{0,0,1,1,0}, 'U'}, 
+  {{0,0,1,1,0}, 'V'}, 
+  {{0,1,1,1,0}, 'W'}, 
+  {{0,0,0,1,0}, 'X'}, 
+  {{1,0,0,0,1}, 'Y'}, 
+  {{0,0,0,1,0}, 'Z'}
 };
 
 const int LDR1 = A0;
@@ -54,18 +55,20 @@ void setup() {
   Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
+
 void loop() {
+  //LEITURA DOS PINOS
   int valor_LDR1 = analogRead(LDR1);
   int valor_LDR2 = analogRead(LDR2);
   int valor_LDR3 = analogRead(LDR3);
   int valor_LDR4 = analogRead(LDR4);
   int valor_LDR5 = analogRead(LDR5);
-  (valor_LDR1 > 900) ? Saida[0] = 0 : Saida[0] = 1;
-  (valor_LDR2 > 900) ? Saida[1] = 0 : Saida[1] = 1;
-  (valor_LDR3 > 900) ? Saida[2] = 0 : Saida[2] = 1;
-  (valor_LDR4 > 900) ? Saida[3] = 0 : Saida[3] = 1;
-  (valor_LDR5 > 900) ? Saida[4] = 0 : Saida[4] = 1;
+ //METRICA PARA DECIDIR QUANDO DEDO ESTA ABAIXADO OU LEVANTADO (0 OU 1)
+  (valor_LDR1 > 1005) ? Saida[0] = 0 : Saida[0] = 1;
+  (valor_LDR2 > 1005) ? Saida[1] = 0 : Saida[1] = 1;
+  (valor_LDR3 > 1005) ? Saida[2] = 0 : Saida[2] = 1;
+  (valor_LDR4 > 1005) ? Saida[3] = 0 : Saida[3] = 1;
+  (valor_LDR5 > 1005) ? Saida[4] = 0 : Saida[4] = 1;
   Serial.print("Codigo: ");
   Serial.print(Saida[0]);
   Serial.print(Saida[1]);
@@ -73,6 +76,7 @@ void loop() {
   Serial.print(Saida[3]);
   Serial.print(Saida[4]);
   Serial.print("\n=============\n");
+  //PRINTA APENAS LETRAS CORRESPONDENTES
   for(int i=0;i<26;i++){
     if(comparaArrays(Saida, Alfabeto[i].chave, 5)){
       Serial.print("Letra: ");
@@ -80,5 +84,5 @@ void loop() {
       Serial.print("\n");
     }
   }
-  delay(3000);
+  delay(1000);
 }
